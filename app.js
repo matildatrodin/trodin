f2dd158f3fb55dec6f59bbf4fbcfe840bd8340f0
matildatrodin <matildatrodin@gmail.com>
2020-05-12 12:01:37 +0200

    added media buttons and color theme picker

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-function themePicker(color) {
-    document.getElementById("body-container-pink").className = color;
+function themePicker(chosenTheme) {
+    current = document.getElementById("body-top-container").className;
+
+    if (current === "body-top-container-" + chosenTheme) {
+        return;
+    } else {
+        document.getElementById("body-top-container").className = "body-top-container-" + chosenTheme;
+        document.getElementById("triangle-right").className = "triangle-right-" + chosenTheme;
+        document.getElementById("triangle-left").className = "triangle-left-" + chosenTheme;
+        document.getElementById("triangle-top-left").className = "triangle-top-left-" + chosenTheme;
+        document.getElementById("header-2").className = "header-2-" + chosenTheme;
+    }
 };
